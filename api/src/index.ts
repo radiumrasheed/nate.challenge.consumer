6b14f1b9a316b22f59cf49df0ccc98358db78aed
Rasheed Rahman <radiumrasheed@gmail.com>
2022-07-20 00:40:36 +0100

    move product constant into it's own file;
    define interface for product

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,35 +1,17 @@
 import cors from 'cors';
-import express, { Response } from 'express';
+import express, {Response} from 'express';
 import bodyParser from 'body-parser';
+import {PRODUCTS} from './products';
+import {executePurchase} from './lib/purchase-api';
 
 const app = express();
 const port = 8080;
-
-const products = [
-  {
-    productId: 1,
-    productUrl: "https://www.bedbathandbeyond.com/store/product/kate-spade-new-york-2-slice-toaster/5374432",
-    productPrice: 9499,
-    discountCode: "FREE5FROMNATE"
-  },
-  {
-    productId: 2,
-    productUrl: "https://www.oculus.com/quest-2/",
-    productPrice: 29900
-  },
-  {
-    productId: 3,
-    productUrl: "https://www.apple.com/shop/product/MME73AM/A/airpods-3rd-generation",
-    productPrice: 17900,
-    discountCode: "GIVEME50"
-  }
-];
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/products', (_, res: Response) => {
-  res.send(products)
+  res.send(PRODUCTS)
 });
 
 app.listen(port, () => {
